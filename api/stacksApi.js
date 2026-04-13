c48d3b41b596ce0973d91bb10784b458ad0fe68f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:30:57 +0100

    Update stacksApi.js

--- api/stacksApi.js
+++ api/stacksApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { STACKS_API_MAINNET } from "../config.js
-
 export async function getBalances(address) {
   const res = await axios.get(
     `${STACKS_API_MAINNET}/extended/v1/address/${address}/balances`

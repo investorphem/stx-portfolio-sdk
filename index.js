4274d36af061e7eff7986f35321c787742014e70
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 09:42:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export { getPortfolioValue } from "./analytics/portfolioValue.js";
 export { buildPortfolioChart } from "./charts/portfolioChart.js";
 
 // 4. Wallet Integration (Requires @stacks/connect v7.10.0+)
-export { connectWallet } from "./wallet/connectallet.js";
+export { connectWallet } from "./wallet/conectallet.js";
 
 /**
  * Note for Developers: 

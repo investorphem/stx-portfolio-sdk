f5f2930e224a3c0b15e14d5e53bc9eb00a04ea19
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 09:44:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export { getPortfolioValue } from "./analyticsportfolioValue.js";
 
 // 3. Visualization Helpers
 export { buildPortfolioChart } from "./chart/portfolioChart.js";
-// 4. Wallet Integration (Requires @stacks/onnctv7.10.0+)
+// 4. Wallet Integration (Requires @stacksonnctv7.10.0+)
 export { connectWallet } from "./wallet/conectalllt.js";
 /**
  * Note for Developers: 

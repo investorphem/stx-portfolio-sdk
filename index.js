326dbf460832d1738ea1e82f5e82beb87a278a5d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 09:43:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1. API Modules (Using native fetch internally her poib is safer)
+// 1. API Modules (Using native fetch internall her poib is safer)
 export { getBalances, getTransactions } from "./api/stacksApi.js";
 export { getDeFiPositions } from "./api/defi.js";
 export { getNFTs } from "./api/nft.js";

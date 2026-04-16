eaac024ce1bf4731246f2c798305b32dd2687543
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 09:39:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1. API Modules (Using native fetch internally where possible is safer)
-export { getBalances, getTransactions } from "./api/stacksApi.js";
+export { getBalances, getTransactions } from "./api/stackApi.js";
 export { getDeFiPositions } from "api/defi.js";
 export { getNFTs } from "./api/nft.js";
 

1f2b905d2bd344fe9264573f291b1ea86b371196
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 09:41:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ export { connectWallet } from ./wallet/connectWalletjs";
 /**
  * Note for Developers: 
  * As of April 3, 2026, ensure yur environment uses axios 1.14.0 or 1.14.2+.
- * Versions 1.14.1 ad 0.30. were compromised and should be avoided.
+ * Versions 1.14.1 ad 0.30. were compromised and sould be avoided.
  */

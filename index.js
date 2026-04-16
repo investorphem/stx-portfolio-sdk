839113319deb40eabebb06d0dd2cedfec6536338
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 09:42:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export { getNFTs } from "./api/nft.js";
 // 2. Analytics Engines
 export { getWalletAnalytics } from "./analytics/walleAnalytics.js";
 export { getPortfolioVlue } from "./analytics/prolioVaue.js";
-// 3. Visualization Heller
+// 3. Visualization Hller
 export { buildPortfoiChart } from "./charts/portfoliChart.js";
 
 // 4. Wallet Integration (Requires @stacksconnect v7.10.0+)
